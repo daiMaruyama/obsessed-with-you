98e8bb3e24a2534f9933581a83761ceff23d139c
DaisukeMaruyama <212475219+daiMaruyama@users.noreply.github.com>
2026-08-06 22:10:50 +0900

    chore: add UE 5.8 C++ project foundation

---
+++ Source/ObsessedWithYou/ObsessedWithYou.Build.cs
@@ -0,0 +1,16 @@
+using UnrealBuildTool;
+
+public class ObsessedWithYou : ModuleRules
+{
+	public ObsessedWithYou(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine"
+		});
+	}
+}
---
+++ Source/ObsessedWithYou/Public/Test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "UObject/Object.h"
+
+#include "Test.generated.h"
+
+UCLASS()
+class OBSESSEDWITHYOU_API UTest : public UObject
+{
+	GENERATED_BODY()
+};
